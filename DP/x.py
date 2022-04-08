b797561d6e09a8204d0d089c71da795206cd0044
leehanjun506 <sis03017@naver.com>
2022-04-08 14:55:10 +0900

    a

--- DP/x.py
+++
@@ -1,2 +0,0 @@
-a=(lambda x : x[0])([1,2,3])
-print(a)
--- 정렬/2751.py
+++
@@ -1,9 +0,0 @@
-num = int(input())
-list = []
-for i in range(0,num):
-    list.append(int(input()))
-
-list.sort()
-
-for i in list:
-    print(i)
