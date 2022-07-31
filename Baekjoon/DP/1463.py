9878b0a637b558fe821735a6bc75dbdd0539c29a
leehanjun506 <sis03017@naver.com>
2022-07-31 15:47:30 +0900

    dp

--- Baekjoon/DP/1463.py
+++ Baekjoon/DP/1463.py
@@ -12,3 +12,13 @@ for i in range(2,num+1):
         dp[i]=min(dp[i//2]+1,dp[i])
     
 print(dp[i])
+
+a = [[1,1],[1,1]]
+b = [[1,1],[1,1]]
+
+c = [[0,0],[0,0]]
+
+for i in range(2):
+    for j in range(2):
+        for k in range(2):
+            c[i,j] = a[i,j]*b[i*k] + d
