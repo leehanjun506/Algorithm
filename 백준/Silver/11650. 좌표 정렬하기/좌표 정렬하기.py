5bb552aa4a68995748655b515e2c6aa371332488
leehanjun506 <sis03017@naver.com>
2022-12-22 21:28:20 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 752 ms, Memory: 142452 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,10 @@
+num =int(input())
+list = []
+for i in range(num):
+    a,b = input().split()
+    a=int(a)
+    b=int(b)
+    list.append([a,b])
+list.sort()
+for i in range(num):
+    print(list[i][0],list[i][1])
