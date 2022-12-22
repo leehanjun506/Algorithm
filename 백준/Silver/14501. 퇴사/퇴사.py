810732a4dc5106f9e4d45475d5c750dfd198aa80
leehanjun506 <sis03017@naver.com>
2022-12-22 21:30:24 +0900

    [Silver III] Title: 퇴사, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,22 @@
+#백준 14501
+n = int(input())
+t = []
+p = []
+dp = [0]*(n+1)
+max_value = 0
+
+for i in range(n):
+    x,y = map(int,input().split())
+    t.append(x)
+    p.append(y)
+for i in range(n-1,-1,-1):
+    time = t[i]+i
+    
+    #상담이 기간안에 끝나는 경우
+    if time <= n:
+        
+        dp[i] = max(p[i]+dp[time],max_value)
+        max_value = dp[i]
+    else:
+        dp[i] = max_value
+print(dp[0])
