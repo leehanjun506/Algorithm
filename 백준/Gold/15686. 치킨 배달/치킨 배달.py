6800f74db046d488b73fdef4aa5c787aa4f4fe61
leehanjun506 <sis03017@naver.com>
2022-12-22 21:31:42 +0900

    [Gold V] Title: 치킨 배달, Time: 160 ms, Memory: 115500 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,36 @@
+# 백준 15686
+from itertools import combinations
+
+bhc = []
+home = []
+n,m = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 2:
+            bhc.append((i,j))
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            home.append((i,j))
+
+def distance(x,y):
+    dis = abs(x[0]-y[0]) + abs(x[1]-y[1])
+    return dis
+
+ans_list = []
+x = [((0,1),(4,4))]
+
+not_close_bhc = list(combinations(bhc,m))
+for i in not_close_bhc: # i는 조합의 개수
+    ans = 0
+    for k in home:
+        min_v = int(1e9)
+        for j in i:
+            min_v = min(min_v,distance(j,k))
+        ans+=min_v
+    ans_list.append(ans)
+ans_list.sort()
+print(ans_list[0])
