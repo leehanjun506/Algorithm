e79b1b8379c8f7b1e7cf9efc13d7938a34af91ca
leehanjun506 <sis03017@naver.com>
2022-12-22 21:22:47 +0900

    [Silver IV] Title: 스택, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,25 @@
+import sys
+n = int(sys.stdin.readline())
+list = []
+for i in range(n):
+    command = sys.stdin.readline()
+    if 'push' in command:
+        list.append(int(command.replace('push ','')))
+    if 'pop' in command:
+        if len(list) == 0:
+            print(-1)
+        else :
+            print(list.pop())
+    if 'size' in command:
+        print(len(list))
+    if 'empty' in command:
+        if len(list) == 0:
+            print(1)
+        else :
+            print(0)
+    if 'top' in command:
+        if len(list) == 0:
+            print(-1)
+        else :
+            print(list[-1])
+        
