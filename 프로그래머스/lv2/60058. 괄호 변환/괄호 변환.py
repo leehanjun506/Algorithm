83afb65e3d38f8e224da4e263ad858e9f719d235
leehanjun506 <sis03017@naver.com>
2023-01-01 11:08:23 +0900

    [level 2] Title: 괄호 변환, Time: 0.31 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,59 @@
+def solution(p):
+    
+    def correct(string):
+        if len(string) == 0:
+             return True
+        list = []
+        if string[0] == ')':
+            return False
+        else:
+            list.append(string[0])
+        for i in range(1,len(string)):
+            if string[i] == '(':
+                list.append(string[i])
+            else:
+                if len(list) == 0:
+                    return False
+                list.pop()
+        if len(list) != 0:
+            return False
+        return True
+    def uandv(string):
+        x,y=0,0
+        u_list = []
+        v_list = []
+        for i in range(len(string)):
+            if string[i]=='(':
+                x+=1
+            else:
+                y+=1
+            if x==y:
+                u_list = string[0:i+1]
+                v_list = string[i+1:]
+                return u_list,v_list
+    def recursive(p):
+        if len(p) == 0:
+            return p
+        u,v = uandv(p) # )(  ,  ' '
+        if correct(u) == True:
+            if correct(v)==True:
+                u+=v
+                return u
+            else:
+                return u+recursive(v)
+        else:
+            empty = ''
+            empty +='('
+            empty +=recursive(v)
+            empty +=')'
+            u=u[1:]
+            u=u[:-1]
+            u=u.replace('(','x')
+            u=u.replace(')','y')
+            u=u.replace('x',')')
+            u=u.replace('y','(')
+            return empty+u
+            
+    ans = recursive(p)
+            
+    return ans
