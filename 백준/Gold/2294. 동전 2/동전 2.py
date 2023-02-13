d48659b14ccd345e12bbabe00cb7df7a645ab087
leehanjun506 <sis03017@naver.com>
2023-02-13 15:09:24 +0900

    [Gold V] Title: 동전 2, Time: 396 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,16 +1,16 @@
-a,b = list(map(int,input().split()))
+n,m = list(map(int,input().split()))
+
 array = []
-for i in range(a):
+for i in range(n):
     array.append(int(input()))
-
-dp = [10001]*(b+1)
+    
+dp = [10001]*(m+1)
 dp[0]=0
+for i in range(n):
+    for j in range(array[i],m+1):
+        dp[j] = min(dp[j],dp[j-array[i]]+1)
 
-for i in range(a):
-    for j in range(array[i],b+1):
-        dp[j]= min(dp[j],dp[j-array[i]]+1)
-
-if dp[b]==10001:
+if dp[m] == 10001:
     print(-1)
-else:
-    print(dp[b])
+else :
+    print(dp[m])
