df71040ee24d5be2bd14d509dd0d9e22f9d17572
leehanjun506 <sis03017@naver.com>
2023-04-02 10:41:39 +0900

    [Gold V] Title: 합분해, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,5 +1,10 @@
 n,k = map(int,input().split())
 dp = [[0]*k for _ in range(n)]
+# n,k 최대값은 0+ (n k-1로) , 1 + (n-1 k-1로) , 2 + (n-2 k-1로)
+# dp[n][k] = dp[0][k-1] + dp[1][k-1] + dp[2][k-2] + ... dp[n][k-1]
+# dp[n-1][k] = dp[0][k-1] + ... dp[n-1][k-1]
+
+# dp[n][k] = dp[n-1][k] + dp[n]
 for i in range(n):
     dp[i][0] = 1
 for j in range(k):
