1c09fa3cd6a32000abeed292eb8aa98120f6cec0
leehanjun506 <sis03017@naver.com>
2023-05-08 04:16:23 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,19 +1,7 @@
 def solution(str_list):
-    s = ''.join(str_list)
-    l = s.find('l')
-    r = s.find('r')
-    # print(l,r)
-    if l == -1 and r == -1:
-        return []
-    if r == -1 and l != -1:
-        if l == 0 :
-            return []
-        return str_list[:l]
-    if l == -1 and r != -1:
-        if r == len(str_list)-1 :
-            return []
-        return str_list[r+1:]
-    if l<r:
-        return str_list[:l]
-    else:
-        return str_list[r+1:]
+    for i in range(len(str_list)):
+        if str_list[i] == 'l':
+            return str_list[:i]
+        if str_list[i] == 'r':
+            return str_list[i+1:]
+    return []
