fe65a6e24b2c6c707e5e4c7d7bfc7f4c4fc65f21
leehanjun506 <sis03017@naver.com>
2023-07-01 01:22:54 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT board.title,board.board_id,reply.reply_id,reply.writer_id,reply.contents,date_format(reply.created_date,'%Y-%m-%d') as created_date from used_goods_board as board join used_goods_reply as reply on board.board_id = reply.board_id where board.created_date like '2022-10%' order by reply.created_date asc,board.title asc
