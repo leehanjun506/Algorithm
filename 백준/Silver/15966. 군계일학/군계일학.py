d670171bb6ef594bf4fc125307692a3d399423dd
leehanjun506 <sis03017@naver.com>
2023-10-13 05:55:25 +0900

    [Silver I] Title: 군계일학, Time: 140 ms, Memory: 136516 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15966. 군계일학/군계일학.py
+++ 백준/Silver/15966. 군계일학/군계일학.py
@@ -2,7 +2,9 @@ n = int(input())
 list = list(map(int,input().split()))
 dp =[0]*1000001
 max_v = 0
-for i in list:
-    dp[i] = dp[i-1]+1
-    max_v = max(max_v,dp[i])
+for i in range(0,n):
+    dp[list[i]] = max(dp[list[i]],dp[list[i]-1]+1)
+    max_v = max(max_v,dp[list[i]])
+
 print(max_v)   
+    
